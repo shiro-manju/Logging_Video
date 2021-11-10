439445752ac21c13fb2658b7211f0eea1da14ca1
shiro-manju <kzk72.ics@gmail.com>
2021-11-10 11:10:51 +0900

    add source

---
+++ pycode/README.md
@@ -0,0 +1,91 @@
+# GStreamerの導入
+
+ひとまず`gst-launch`を使ってUSBカメラの表示と動画保存を行う。
+
+## インストール
+
+```console
+$ sudo apt install gstreamer1.0-*
+```
+
+自分の環境だと`gstreamer1.0-vaapi`のインストールに失敗するので、個別にインストールする。
+
+```console
+$ sudo apt install -y gstreamer1.0-alsa gstreamer1.0-fluendo-mp3 gstreamer1.0-plugins-bad gstreamer1.0-plugins-base-dbg gstreamer1.0-plugins-ugly gstreamer1.0-rtsp gstreamer1.0-x \
+gstreamer1.0-clutter-3.0 gstreamer1.0-libav gstreamer1.0-plugins-bad-dbg gstreamer1.0-plugins-base-doc gstreamer1.0-plugins-ugly-dbg gstreamer1.0-rtsp-dbg \
+gstreamer1.0-crystalhd gstreamer1.0-libav-dbg gstreamer1.0-plugins-bad-doc gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly-doc gstreamer1.0-tools \
+gstreamer1.0-doc gstreamer1.0-nice gstreamer1.0-plugins-base gstreamer1.0-plugins-good-dbg gstreamer1.0-pocketsphinx \
+gstreamer1.0-espeak gstreamer1.0-packagekit gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-good-doc gstreamer1.0-pulseaudio
+```
+
+## Webカメラの表示と保存
+
+表示
+
+```console
+$ gst-launch-1.0 v4l2src ! videoconvert ! ximagesink
+```
+
+保存
+
+```console
+$ gst-launch-1.0 v4l2src num-buffers=500 \
+! queue \
+! x264enc \
+! mp4mux \
+! filesink location=video.mp4
+```
+
+## その他
+
+### カメラ接続（VMware）
+
+VMwareでカメラが接続できない場合は、接続設定を行った上で再起動を行うと接続できた。（はじめから接続設定がON状態の状態でVMwareを起動する。）
+
+### 参考URL
+
+[http://littlewing.hatenablog.com/entry/2016/02/24/200129](http://littlewing.hatenablog.com/entry/2016/02/24/200129)
+
+# C言語からGSdtreamerを呼び出す
+
+## インストール
+
+```console
+$ sudo apt install -y libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools
+```
+
+## その他
+
+### 参考URL
+
+[https://gstreamer.freedesktop.org/documentation/tutorials/basic/hello-world.html](https://gstreamer.freedesktop.org/documentation/tutorials/basic/hello-world.html)
+
+# PythonからGStreamerを呼び出す
+
+動的にパイプを組み替えたい（逆走検知時に別に動画を保存したい）ために、gst-launchを直接使うのではなくPythonのコードからGStreamerを使用したい。
+
+## インストール
+
+```console
+$ sudo apt install -y libgirepository1.0-dev
+$ pip install pygobject
+```
+
+## サンプルコード実行
+
+```console
+$ python3 hello.py
+```
+
+## USB画像層をMP4に保存するコード
+
+```console
+$ python3 usbcam_save.py
+```
+
+## その他
+
+### 参考URL
+
+[http://brettviren.github.io/pygst-tutorial-org/pygst-tutorial.html](http://brettviren.github.io/pygst-tutorial-org/pygst-tutorial.html)  
+[https://lazka.github.io/pgi-docs/Gst-1.0/classes.html](https://lazka.github.io/pgi-docs/Gst-1.0/classes.html)
---
+++ pycode/py/usbcam_multi_save.py
@@ -0,0 +1,110 @@
+import sys
+from datetime import datetime as dt
+
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst, GLib
+
+osel_src1 = None
+osel_src2 = None
+
+SWITCH_TIMEOUT_SEC = 10
+
+
+def my_bus_callback(bus, message, loop):
+    t = message.type
+    if t == Gst.MessageType.ERROR:
+        err, debug = message.parse_error()
+        print('Error: {}'.format(err))
+        loop.quit()
+    elif t == Gst.MessageType.EOS:
+        loop.quit()
+    else:
+        # print(t)
+        pass
+    return True
+
+
+def switch_cb(user_data):
+    sel = user_data
+    old_pad = sel.get_property('active-pad')
+    if old_pad == osel_src1:
+        new_pad = osel_src2
+    else:
+        new_pad = osel_src1
+    sel.set_property('active-pad', new_pad)
+    return True
+
+
+def on_bin_element_added(bin, element):
+    element.set_property('sync', False)
+    element.set_property('async', False)
+
+
+def main():
+    global osel_src1
+    global osel_src2
+
+    Gst.init(None)
+    loop = GLib.MainLoop().new(None, False)
+
+    pipline = Gst.Pipeline.new('pipeline')
+
+    src = Gst.ElementFactory.make('videotestsrc', 'src')
+    c0 = Gst.ElementFactory.make('videoconvert', None)
+    toverlay = Gst.ElementFactory.make('timeoverlay', 'timeoverlay')
+    enc = Gst.ElementFactory.make('x264enc', 'enc')
+    osel = Gst.ElementFactory.make('output-selector', 'osel')
+    sink1 = Gst.ElementFactory.make('filesink', 'sink1')
+    sink2 = Gst.ElementFactory.make('filesink', 'sink2')
+
+    if not pipline or not src or not c0 or not toverlay or not osel or not sink1 or not sink2:
+        sys.exit('missing element')
+
+    pipline.add(src)
+    pipline.add(c0)
+    pipline.add(toverlay)
+    pipline.add(enc)
+    pipline.add(osel)
+    pipline.add(sink1)
+    pipline.add(sink2)
+
+    osel.set_property('resend-latest', True)
+    sink1.set_property('location', dt.now().strftime('%Y_%m_%d') + '_1.mp4')
+    sink2.set_property('location', dt.now().strftime('%Y_%m_%d') + '_2.mp4')
+    sink1.set_property('sync', False)
+    sink2.set_property('sync', False)
+    sink1.set_property('async', False)
+    sink2.set_property('async', False)
+
+    src.link(c0)
+    c0.link(toverlay)
+    toverlay.link(enc)
+    enc.link(osel)
+
+    sinkpad = sink1.get_static_pad('sink')
+    osel_src1 = osel.get_request_pad('src_%u')
+
+    if osel_src1.link(sinkpad) != Gst.PadLinkReturn.OK:
+        sys.exit('linking output 1 converter failed')
+
+    sinkpad = sink2.get_static_pad('sink')
+    osel_src2 = osel.get_request_pad('src_%u')
+
+    if osel_src2.link(sinkpad) != Gst.PadLinkReturn.OK:
+        sys.exit('linking output 2 converter failed')
+
+    GObject.timeout_add_seconds(SWITCH_TIMEOUT_SEC, switch_cb, osel)
+
+    bus = pipline.get_bus()
+    bus.add_watch(0, my_bus_callback, loop)
+
+    pipline.set_state(Gst.State.PLAYING)
+
+    loop.run()
+
+    pipline.set_state(Gst.State.NULL)
+
+
+if __name__ == '__main__':
+    main()
